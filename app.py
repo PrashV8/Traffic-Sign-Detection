608cd854dd432020c53cb3a4f781817cee166bd2
PrashV8 <prashantvr8@gmail.com>
2024-07-10 12:15:25 +0530

    Initial commitone

---
+++ app.py
@@ -0,0 +1,177 @@
+from __future__ import division, print_function
+import sys
+import os
+import glob
+import re
+import numpy as np
+import tensorflow as tf
+import cv2
+
+import tempfile
+from flask import send_file
+import io
+import base64
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from tensorflow.keras.models import load_model
+
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+MODEL_PATH ='model.h5'
+
+model = load_model(MODEL_PATH)
+
+def grayscale(img):
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    return img
+
+def equalize(img):
+    img = cv2.equalizeHist(img)
+    return img
+
+def preprocessing(img):
+    img = grayscale(img)
+    img = equalize(img)
+    img = img/255
+    return img
+
+def getClassName(classNo):
+    if classNo == 0: return 'Speed Limit 20 km/h'
+    elif classNo == 1: return 'Speed Limit 30 km/h'
+    elif classNo == 2: return 'Speed Limit 50 km/h'
+    elif classNo == 3: return 'Speed Limit 60 km/h'
+    elif classNo == 4: return 'Speed Limit 70 km/h'
+    elif classNo == 5: return 'Speed Limit 80 km/h'
+    elif classNo == 6: return 'End of Speed Limit 80 km/h'
+    elif classNo == 7: return 'Speed Limit 100 km/h'
+    elif classNo == 8: return 'Speed Limit 120 km/h'
+    elif classNo == 9: return 'No passing'
+    elif classNo == 10: return 'No passing for vehicles over 3.5 metric tons'
+    elif classNo == 11: return 'Right-of-way at the next intersection'
+    elif classNo == 12: return 'Priority road'
+    elif classNo == 13: return 'Yield'
+    elif classNo == 14: return 'Stop'
+    elif classNo == 15: return 'No vehicles'
+    elif classNo == 16: return 'Vehicles over 3.5 metric tons prohibited'
+    elif classNo == 17: return 'No entry'
+    elif classNo == 18: return 'General caution'
+    elif classNo == 19: return 'Dangerous curve to the left'
+    elif classNo == 20: return 'Dangerous curve to the right'
+    elif classNo == 21: return 'Double curve'
+    elif classNo == 22: return 'Bumpy road'
+    elif classNo == 23: return 'Slippery road'
+    elif classNo == 24: return 'Road narrows on the right'
+    elif classNo == 25: return 'Road work'
+    elif classNo == 26: return 'Traffic signals'
+    elif classNo == 27: return 'Pedestrians'
+    elif classNo == 28: return 'Children crossing'
+    elif classNo == 29: return 'Bicycles crossing'
+    elif classNo == 30: return 'Beware of ice/snow'
+    elif classNo == 31: return 'Wild animals crossing'
+    elif classNo == 32: return 'End of all speed and passing limits'
+    elif classNo == 33: return 'Turn right ahead'
+    elif classNo == 34: return 'Turn left ahead'
+    elif classNo == 35: return 'Ahead only'
+    elif classNo == 36: return 'Go straight or right'
+    elif classNo == 37: return 'Go straight or left'
+    elif classNo == 38: return 'Keep right'
+    elif classNo == 39: return 'Keep left'
+    elif classNo == 40: return 'Roundabout mandatory'
+    elif classNo == 41: return 'End of no passing'
+    elif classNo == 42: return 'End of no passing by vehicles over 3.5 metric tons'
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(32, 32))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0  # Normalize the image data
+
+    # Make predictions
+    predictions = model.predict(img_array)
+    predicted_class = np.argmax(predictions)
+
+    # Get the class name
+    preds = getClassName(predicted_class)
+    return preds
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        predicted_class = model_predict(file_path, model)
+        
+        # Return the predicted class name as a response
+        return predicted_class
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
